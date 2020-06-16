ae9916c8fa23ecce2aa63d543db3a2bacbf34aff
samsonmking <samson@samsonmking.com>
2020-06-16 02:45:24 +0100

    Forwarding keypresses to change book pages

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ const app = express();
 const epaper = require('bindings')('epaper.node');
 const puppeteer = require('puppeteer-core');
 const convertPNGto1BitBuffer = require('./image.js');
+const readline = require('readline');
 
 const height = 300;
 const width = 400;
@@ -16,15 +17,25 @@ async function renderBrowser() {
         height,
         deviceScaleFactor: 1
     });
+    page.on('console', msg => console.log('PAGE LOG:', msg.text));
     await page.goto('http://localhost:3000/index.html');
     await page.waitForFunction(() => window.ebookDisplayed === true);
-    const pageImage = await page.screenshot({
-        type: 'png',
-        fullpage: 'true',
-        encoding: 'binary'
+    await display(page);
+
+    pageCount = 0;
+    process.stdin.addListener('keypress', async (key, data) => {
+        if(data.name === 'left') {
+            page.keyboard.press('ArrowLeft');
+            await page.waitForFunction(pc => window.pageCount === pc, {}, --pageCount);
+            await display(page);
+        }
+        if(data.name === 'right') {
+            page.keyboard.press('ArrowRight');
+            await page.waitForFunction(pc => window.pageCount === pc, {}, ++pageCount);
+            await display(page);
+        }
     });
-    const displayBuffer = await convertPNGto1BitBuffer(pageImage);
-    display(displayBuffer);
+
 }
 
 function setUpDisplay() {
@@ -32,10 +43,23 @@ function setUpDisplay() {
     epaper.init();
 }
 
-function display(buffer) {
-    epaper.display(buffer);
-    epaper.sleep();
+async function display(page) {
+    const pageImage = await page.screenshot({
+        type: 'png',
+        fullpage: 'true',
+        encoding: 'binary'
+    });
+    const displayBuffer = await convertPNGto1BitBuffer(pageImage);
+    epaper.display(displayBuffer);
 }
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.addListener('keypress', (key, data) => {
+    if(data.ctrl && data.name === 'c') {
+        process.exit();
+    }
+});
 
 app.use(express.static('static'));
 app.listen(3000, () => {
