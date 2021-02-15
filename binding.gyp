663a7799af8e76ea694724b3d390290ce352357c
ascension <jerroddb@gmail.com>
2021-02-14 21:05:12 -0700

    add bindings and node module for 7.5b HD

--- binding.gyp
+++ binding.gyp
@@ -43,6 +43,28 @@
                 "-lwiringPi",
                 "-lm"
             ]
+        },
+        {
+            "target_name": "waveshare7in5bHD",
+            "cflags!": [ "-fno-exceptions" ],
+            "cflags_cc!": [ "-fno-exceptions" ],
+            "sources": [ 
+                "./c/EPD_7in5b_HD_node.cc",
+                "./c/DEV_Config.c",
+                "./c/EPD_7in5b_HD.c"
+            ],
+            "defines": [
+                "RPI",
+                "USE_WIRINGPI_LIB",
+                "NAPI_DISABLE_CPP_EXCEPTIONS"
+            ],
+            "include_dirs": [
+                "<!@(node -p \"require('node-addon-api').include\")"
+            ],
+            "libraries": [
+                "-lwiringPi",
+                "-lm"
+            ]
         }
     ]
 }
--- devices.js
+++ devices.js
@@ -1,6 +1,7 @@
 const image = require('./image.js');
 const waveshare4In2Driver = require('bindings')('waveshare4in2.node');
 const waveshare7in5v2Driver = require('bindings')('waveshare7in5v2.node');
+const waveshare7in5bHDDriver = require('bindings')('waveshare7in5bHD.node');
 
 const waveshare4in2Horizontal = {
     height: 300,
@@ -80,6 +81,32 @@ const waveshare7in2v2Vertical = {
     },
 };
 
+const waveshare7in5bHDHorizontal = {
+    height: 528,
+    width: 880,
+    driver: waveshare7in5bHDDriver,
+    displayPNG: async function (imgContents) {
+        const buffer = await image.convertPNGto1BitBW(imgContents);
+        this.driver.display(buffer);
+    },
+    init: function () {
+        this.driver.init();
+    },
+};
+
+const waveshare7in2bHDVertical = {
+    height: 880,
+    width: 528,
+    driver: waveshare7in5bHDDriver,
+    displayPNG: async function (imgContents) {
+        const buffer = await image.convertPNGto1BitBWRotated(imgContents);
+        this.driver.display(buffer);
+    },
+    init: function () {
+        this.driver.init();
+    },
+};
+
 const devices = {
     // default waveshare4in2 kept for backwards compatibility with release 1.0.0
     waveshare4in2: waveshare4in2Horizontal,
@@ -91,6 +118,10 @@ const devices = {
     waveshare7in5v2: waveshare7in5v2Horizontal,
     waveshare7in5v2Horizontal,
     waveshare7in2v2Vertical,
+    // default waveshare7in5bHD kept for backwards compatibility with releaes 1.1.0
+    waveshare7in5bHD: waveshare7in5bHDHorizontal,
+    waveshare7in5bHDHorizontal,
+    waveshare7in2bHDVertical,
 };
 
 module.exports = devices;
