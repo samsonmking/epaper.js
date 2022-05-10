b34e25d03d9455e09ee521ebbb70d62c8c076aaf
Mathieu Leguey <mathieu.leguey@gmail.com>
2022-05-10 15:03:51 +0200

    .

--- packages/rpi-it8951/src/c/EPD_IT8951.c
+++ packages/rpi-it8951/src/c/EPD_IT8951.c
@@ -188,6 +188,7 @@ parameter:  data
 ******************************************************************************/
 static void EPD_IT8951_ReadMultiData(UWORD *Data_Buf, UDOUBLE Length)
 {
+    uint8_t buf[2];
     UWORD Write_Preamble = 0x1000;
     UWORD Read_Dummy;
 
@@ -208,12 +209,14 @@ static void EPD_IT8951_ReadMultiData(UWORD *Data_Buf, UDOUBLE Length)
 
     EPD_IT8951_ReadBusy();
 
-    DEV_SPI_ReadBytes((uint8_t *)Data_Buf, Length * 2);
-    /*for (UDOUBLE i = 0; i < Length; i++)
+    // DEV_SPI_ReadBytes((uint8_t *)Data_Buf, Length * 2);
+    for (UDOUBLE i = 0; i < Length; i++)
     {
-        Data_Buf[i] = DEV_SPI_ReadByte(0) << 8;
-        Data_Buf[i] |= DEV_SPI_WriteByte(0);
-    }*/
+        memset(buf, 0, 2);
+        DEV_SPI_ReadBytes(&buf[0], 2);
+        Data_Buf[i] = (UWORD)buf[0] << 8;
+        Data_Buf[i] |= (UWORD)buf[1];
+    }
 
     DEV_Digital_Write(EPD_CS_PIN, 1);
 }
