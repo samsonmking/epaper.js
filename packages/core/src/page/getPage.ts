3b1f5c7f414adeb438790637894e9a4222ddca0e
Mathieu Leguey <mathieu.leguey@gmail.com>
2022-05-11 01:44:42 +0200

    .

--- packages/core/src/page/getPage.ts
+++ packages/core/src/page/getPage.ts
@@ -5,7 +5,8 @@ import { BrowserPage } from './page';
 export async function getPageRpi(width: number, height: number, logger?: Logger) {
     const browser = await puppeteer.launch({
         executablePath: 'chromium-browser',
-        args: ['--font-render-hinting=slight'],
+        headless: true,
+        args: ['--font-render-hinting=slight', '--headless', '--no-sandbox', '--disable-setuid-sandbox'],
     });
     const context = await browser.createIncognitoBrowserContext();
     const browserPage = await context.newPage();
